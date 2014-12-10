869bc8a3b57c1603d22f147eed1a426352980fb6
Randolph Higgins <higginsrandolph78@gmail.com>
2014-12-10 17:19:36 -0500

    added celcius to farenheit converter

--- 05Window/Window2.java
+++ 05Window/Window2.java
@@ -1,32 +1,48 @@
 
 import javax.swing.*;
 import java.awt.*;
-public class Window2 extends JFrame {
-private Container pane;
+import java.awt.event.*;
+public class Window2 extends JFrame implements ActionListener{
+    private Container pane;
 
-private JButton b;
-private JLabel l;
-private JTextField t;
-private JCheckBox c;
+    private JButton b;
+    private JButton cel;
+    private JLabel l;
+    private JTextField t;
     public Window2() {
-this.setTitle("My first GUI");
-this.setSize(600,400);
-this.setLocation(100,100);
-this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	this.setTitle("Temperature converter");
+	this.setSize(600,100);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 
-pane = this.getContentPane();
-pane.setLayout(new FlowLayout());
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
 
-b = new JButton("Do Nothing");
-l = new JLabel("This is AWESOME! (lies)",null,JLabel.CENTER);
-t = new JTextField(12);
-c = new JCheckBox("Overclock!!!");
-pane.add(l);
-pane.add(b);
-pane.add(t);
-pane.add(c);
-}
+	b = new JButton("Farenheit to Celcius");
+	cel = new JButton("Celcius to Farenheit");
+	l = new JLabel("hey, buddy, wanna convert temperatures?"+"\n",null,JLabel.CENTER);
+	t = new JTextField(12);
+	pane.add(l);
+	pane.add(b);
+	pane.add(cel);
+	pane.add(t);
+	t.setText("0");
+	b.addActionListener(this);
+	b.setActionCommand("toCel");
+	cel.addActionListener(this);
+	cel.setActionCommand("toFar");
+    }
+
+    public void actionPerformed(ActionEvent e){
+	float x = Float.parseFloat(t.getText());
+	if(e.getActionCommand() == "toCel"){
+	    t.setText(""+((x - 32)/1.8));
+	}
+	if(e.getActionCommand() == "toFar"){
+	    t.setText(""+((1.8*x) + 32));
+	}
+    }
 
     public static void main(String[] args) {
         Window2 g = new Window2();
